d7d5a7c41ed8cacb8285d5370e84f2592ae508f8
mihantsa19 <mihantsarajaonarivelo@gmail.com>
2025-07-14 12:07:02 +0300

    faran

--- page/login.php
+++ page/login.php
@@ -26,6 +26,8 @@ if (isset($_POST['email'])) {
     <title>Connexion</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           crossorigin="anonymous">
+          <link rel="stylesheet" href="../css/style.css" />
+
 </head>
 <body class="bg-light">
 
